c217f754842e077f430d847d07b3663d94bf194f
Davide Saporita <davide.saporita@gmail.com>
2020-05-29 16:54:41 +0200

    Added action buttons

--- show.php
+++ show.php
@@ -11,7 +11,12 @@ include __DIR__ . '/partials/templates/head.php';
             <img src="https://room-matehotels.com/images/img/oscar/hotel/hotel09.jpg" alt="" class="card-img-top">
             <h3 class="card-title"><?php echo "Stanza N° {$room['room_number']}"; ?></h3>
             <p class="card-text">La stanza <?php echo $room['room_number']; ?> possiede un numero di letti pari a <?php echo $room['beds']; ?> e si trova al <?php echo $room['floor']; ?>° piano.</p>
-            <a href="index.php" class="btn btn-primary">Torna all'elenco delle stanze</a>
+            <form action="<?php echo "$base_path/partials/delete/server.php"?>" method="POST">
+                <a href="<?php echo $base_path;?>" class="btn btn-primary">Torna all'elenco delle stanze</a>
+                <a href="<?php echo "$base_path/edit.php?id={$room['id']}";?>" class="btn btn-warning">Modifica</a>
+                <input type="hidden" name="id" value="<?php echo $room['id']; ?>">
+                <input class="btn btn-danger" type="submit" value="Elimina">
+            </form>
         </div>
     </div>
 </main>
