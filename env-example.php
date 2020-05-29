5c2ab9b2b2e9bd1caaf1a9526c0d0ce9531427f2
Davide Saporita <davide.saporita@gmail.com>
2020-05-29 17:14:57 +0200

    Updated with new vars

--- env-example.php
+++ env-example.php
@@ -3,3 +3,8 @@ $server_name = 'server';
 $username = 'user';
 $password = 'password';
 $db_name = 'database';
+
+// App info
+$host = 'http://localhost/';
+$folder = 'foldername';
+$base_path = $host . $folder;
