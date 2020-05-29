e9c3291c9d09d1bbfeed535dd7efcdc51a1104d4
Davide Saporita <davide.saporita@gmail.com>
2020-05-29 16:46:48 +0200

    Fixed buttons style

--- index.php
+++ index.php
@@ -52,8 +52,8 @@ include __DIR__ . '/partials/templates/head.php';
                                 <td><?php echo $room['room_number']; ?></td>
                                 <td><?php echo $room['floor']; ?></td>
                                 <td><?php echo $room['beds']; ?></td>
-                                <td><a class="text-success" href="<?php echo "$base_path/show.php?id={$room['id']}"; ?>">Visualizza</a></td>
-                                <td><a class="text-warning" href="<?php echo "$base_path/edit.php?id={$room['id']}"; ?>">Modifica</a></td>
+                                <td><a class="btn btn-success" href="<?php echo "$base_path/show.php?id={$room['id']}"; ?>">Visualizza</a></td>
+                                <td><a class="btn btn-warning" href="<?php echo "$base_path/edit.php?id={$room['id']}"; ?>">Modifica</a></td>
                                 <td>
                                     <form action="<?php echo "$base_path/partials/delete/server.php"?>"
                                           method="POST">
